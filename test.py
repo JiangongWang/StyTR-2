ae67b9056d90285c18845cc61e0fff4ac0e9a525
1080ti_ubuntu <1080ti@example.com>
2022-09-26 11:17:26 +0800

    resize

--- test.py
+++ test.py
@@ -20,7 +20,7 @@ def test_transform(size, crop):
     transform_list = []
    
     if size != 0: 
-        transform_list.append(transforms.Resize(size))
+        transform_list.append(transforms.Resize((size, size)))
     if crop:
         transform_list.append(transforms.CenterCrop(size))
     transform_list.append(transforms.ToTensor())
